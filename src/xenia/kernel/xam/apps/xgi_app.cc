5f6d8bcfd60ef4532150e510e34cd1d5abf0cb1e
Codie Stella <git@codie.gg>
2023-03-11 02:32:58 +0000

    Gamenight rough patch

--- src/xenia/kernel/xam/apps/xgi_app.cc
+++ src/xenia/kernel/xam/apps/xgi_app.cc
@@ -352,6 +352,9 @@ X_HRESULT XgiApp::DispatchMessageSync(uint32_t message, uint32_t buffer_ptr,
       return X_E_SUCCESS;
     }
     case 0x000B001C: {
+      resetQosCache();
+
+
       XELOGI("XSessionSearchEx");
 
       int i = 0;
@@ -514,6 +517,8 @@ X_HRESULT XgiApp::DispatchMessageSync(uint32_t message, uint32_t buffer_ptr,
         xe::be<uint32_t> unk6;
       }* data = reinterpret_cast<message_data*>(buffer);
 
+            resetQosCache();
+
       XELOGI("XSessionGetDetails({:08X});", buffer_length);
 
       auto details = memory_->TranslateVirtual<XSESSION_LOCAL_DETAILS*>(
@@ -651,6 +656,8 @@ X_HRESULT XgiApp::DispatchMessageSync(uint32_t message, uint32_t buffer_ptr,
         xe::be<uint32_t> session_handle;
         xe::be<uint32_t> session_info;
       }* data = reinterpret_cast<message_data*>(buffer);
+
+            resetQosCache();
 
       XELOGI("XSessionMigrateHost({:08X});", buffer_length);
 
@@ -938,6 +945,9 @@ X_HRESULT XgiApp::DispatchMessageSync(uint32_t message, uint32_t buffer_ptr,
         xe::be<uint32_t> results_buffer;
         xe::be<uint32_t> unk3;
       }* data = reinterpret_cast<message_data*>(buffer);
+      resetQosCache();
+
+
       XELOGI("XSessionArbitrationRegister({:08X}, {:08X}, {:08X}, {:08X}, {:08X}, {:08X}, {:08X}, {:08X});", 
           data->session_handle, data->flags, data->unk1, data->unk2,
           data->session_nonce, data->results_buffer_length,
@@ -1054,6 +1064,9 @@ X_HRESULT XgiApp::DispatchMessageSync(uint32_t message, uint32_t buffer_ptr,
     }
     case 0x000B0006: {
       assert_true(!buffer_length || buffer_length == 24);
+      resetQosCache();
+
+
       // dword r3 user index
       // dword (unwritten?)
       // qword 0
@@ -1097,6 +1110,9 @@ X_HRESULT XgiApp::DispatchMessageSync(uint32_t message, uint32_t buffer_ptr,
         // - XamSessionRefObjByHandle
         // - [this]
         // - CloseHandle
+        resetQosCache();
+
+
         uint32_t session_handle = xe::load_and_swap<uint32_t>(buffer + 0x0);
         uint32_t flags = xe::load_and_swap<uint32_t>(buffer + 0x4);
         uint32_t num_slots_public = xe::load_and_swap<uint32_t>(buffer + 0x8);
@@ -1214,6 +1230,8 @@ X_HRESULT XgiApp::DispatchMessageSync(uint32_t message, uint32_t buffer_ptr,
     #pragma endregion
         } else {
     #pragma region Curl
+          resetQosCache();
+
             /*
                 TODO:
                     - Refactor the CURL out to a separate class.
@@ -1322,6 +1340,9 @@ X_HRESULT XgiApp::DispatchMessageSync(uint32_t message, uint32_t buffer_ptr,
       // TODO(PermaNull): reverse buffer contents.
       XELOGD("XGISessionDelete");
 
+            resetQosCache();
+
+
       struct message_data {
         xe::be<uint32_t> session_handle;
       }* data = reinterpret_cast<message_data*>(buffer);
@@ -1386,6 +1407,9 @@ X_HRESULT XgiApp::DispatchMessageSync(uint32_t message, uint32_t buffer_ptr,
       uint32_t xuid_array = xe::load_and_swap<uint32_t>(buffer + 0x8);
       uint32_t user_index_array = xe::load_and_swap<uint32_t>(buffer + 0xC);
       uint32_t private_slots_array = xe::load_and_swap<uint32_t>(buffer + 0x10);
+
+            resetQosCache();
+
 
       // Local uses user indices, remote uses XUIDs
       if (xuid_array == 0) {
@@ -1485,6 +1509,9 @@ X_HRESULT XgiApp::DispatchMessageSync(uint32_t message, uint32_t buffer_ptr,
       return X_E_SUCCESS;
     }
     case 0x000B0013: {
+      resetQosCache();
+
+
       assert_true(buffer_length == 0x14);
       uint32_t session_ptr = xe::load_and_swap<uint32_t>(buffer + 0x0);
       uint32_t array_count = xe::load_and_swap<uint32_t>(buffer + 0x4);
